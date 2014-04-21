6fe6002a05f873465c28ae873b57f06c9e87f737
Luke Keller <lukeekeller@gmail.com>
2014-04-21 07:24:45 -0400

    Devise for user authentication. Rspec test written to verify user build and presence of email.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.1.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'spring',        group: :development
+gem 'devise'
+
+group :development, :test do
+  gem 'factory_girl_rails'
+  gem 'rspec-rails', '>= 2.14'
+end
+
+group :test do
+  gem 'capybara-webkit', '>= 1.0.0'
+  gem 'shoulda-matchers'
+end
---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Wikidux
+  class Application < Rails::Application
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
+  end
+end
