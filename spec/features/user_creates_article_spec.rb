5c04c5b596399db8402cef56250250dc16940659
Luke Keller <lukeekeller@gmail.com>
2014-04-27 18:54:32 -0400

    integrations testing for article creation

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -2,7 +2,18 @@ require "spec_helper"
 
 feature 'Article creation' do
 
-  pending 'user logs in and creates an article' do
+  scenario 'user logs in and creates an article' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit '/articles/new'
+    fill_in 'Title', with: 'Interesting Title'
+    fill_in 'Content', with: 'Even more interesting article!'
+
+    click_button 'Create Article'
+
+    expect(page).to have_content('Interesting Title')
+
   end
 
 end
